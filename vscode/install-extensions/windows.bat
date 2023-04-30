b3657e1dbfe002037e892bd0600ebd95c188beaa
juanmacivico87 <juanmacivico87@gmail.com>
2023-04-30 13:07:24 +0200

    Initial commit

---
+++ vscode/install-extensions/windows.bat
@@ -0,0 +1,11 @@
+code --install-extension gerardreches.php-extension-pack
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension emallin.phpunit
+code --install-extension wordpresstoolbox.wordpress-toolbox
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension eamodio.gitlens
+code --install-extension gruntfuggly.todo-tree
+
+code --install-extension github.copilot
