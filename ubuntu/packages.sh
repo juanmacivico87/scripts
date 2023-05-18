3649bb72f01d970444939a3f46cd8dbdfc4e691e
juanmacivico87 <juanmacivico87@gmail.com>
2023-05-18 23:19:31 +0200

    Add command to install pip3

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -8,6 +8,7 @@ echo '[AP2] Install Apache'
 echo '[MYSQL] Intall MySQL'
 echo '[MDB] Intall MariaDB'
 echo '[PHP] Install PHP'
+echo '[PIP3] Install PIP3'
 echo '[NVM] Install NVM'
 echo '[COMP] Install Composer'
 echo '[WPCLI] Install WP-CLI'
@@ -48,6 +49,10 @@ case $OPTION in
 
     sudo apt install php"$PHP_VERSION"-common php"$PHP_VERSION"-cli -y
     sudo apt install php"$PHP_VERSION" libapache2-mod-php"$PHP_VERSION" php"$PHP_VERSION"-fpm php-common php"$PHP_VERSION"-mysql php"$PHP_VERSION"-xml php"$PHP_VERSION"-xmlrpc php"$PHP_VERSION"-curl php"$PHP_VERSION"-gd php"$PHP_VERSION"-imagick php"$PHP_VERSION"-cli php"$PHP_VERSION"-dev php"$PHP_VERSION"-imap php"$PHP_VERSION"-mbstring php"$PHP_VERSION"-opcache php"$PHP_VERSION"-readline php"$PHP_VERSION"-soap php"$PHP_VERSION"-zip php"$PHP_VERSION"-intl php"$PHP_VERSION"-xdebug php"$PHP_VERSION"-redis -y
+    ;;
+  PIP3)
+    sudo apt install python3-pip
+    pip3 --version
     ;;
   MYSQL)
     sudo apt install -y mysql-server mysql-client
