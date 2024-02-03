cd98b528e7a77506c68daed422958dace7001b87
juanmacivico87 <juanmacivico87@gmail.com>
2024-02-03 21:15:13 +0100

    Create options to install VSCode extensions

--- vscode/install-extensions/linux.sh
+++ vscode/install-extensions/linux.sh
@@ -1,14 +1,43 @@
 #!/usr/bin/bash
-code --install-extension gerardreches.php-extension-pack
-code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension emallin.phpunit
-code --install-extension wordpresstoolbox.wordpress-toolbox
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension eamodio.gitlens
-code --install-extension gruntfuggly.todo-tree
-code --install-extension ms-python.python
-code --install-extension kevinrose.vsc-python-indent
+echo 'What type of extensions do you want to install??'
+echo ''
+echo '[DEV] Extensions that I can use to develop, regardless of the language I use'
+echo '[PHP] For my PHP developments'
+echo '[PYTHON] For my Python developments'
+echo '[WP] For my WordPress developments'
+echo '[CPP] For my C++ developments'
+read OPTION
 
-code --install-extension github.copilot
+OPTION=${OPTION^^}
+
+case $OPTION in
+  DEV)
+    code --install-extension ms-vscode-remote.remote-ssh
+    code --install-extension ms-vsliveshare.vsliveshare
+    code --install-extension eamodio.gitlens
+    code --install-extension gruntfuggly.todo-tree
+    ;;
+  PHP)
+    code --install-extension gerardreches.php-extension-pack
+    code --install-extension bmewburn.vscode-intelephense-client
+    code --install-extension emallin.phpunit
+    ;;
+  PYTHON)
+    code --install-extension ms-python.python
+    code --install-extension kevinrose.vsc-python-indent
+    ;;
+  WP)
+    code --install-extension wordpresstoolbox.wordpress-toolbox
+    ;;
+  CPP)
+    code --install-extension ms-vscode.cpptools
+    ;;
+  *)
+    echo ''
+    echo -e '\033[1;31m Sorry, that is not a valid option \033[0m'
+    echo ''
+    echo 'Please, press ENTER to exit . . .'
+    read CLOSE
+    exit
+    ;;
+esac
--- vscode/install-extensions/windows.bat
+++ vscode/install-extensions/windows.bat
@@ -1,14 +1,32 @@
-code --install-extension gerardreches.php-extension-pack
-code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension emallin.phpunit
-code --install-extension wordpresstoolbox.wordpress-toolbox
-code --install-extension ms-vscode-remote.remote-ssh
-code --install-extension ms-vscode-remote.remote-wsl
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vsliveshare.vsliveshare
-code --install-extension eamodio.gitlens
-code --install-extension gruntfuggly.todo-tree
-code --install-extension ms-python.python
-code --install-extension kevinrose.vsc-python-indent
+@echo off
 
-code --install-extension github.copilot
+echo [DEV] Extensions that I can use to develop, regardless of the language I use
+echo [PHP] For my PHP developments
+echo [PYTHON] For my Python developments
+echo [WP] For my WordPress developments
+echo [CPP] For my C++ developments
+
+set /p option=What type of extensions do you want to install??
+
+IF "%option%"=="DEV" (
+  code --install-extension ms-vscode-remote.remote-ssh
+  code --install-extension ms-vscode-remote.remote-wsl
+  code --install-extension ms-vsliveshare.vsliveshare
+  code --install-extension eamodio.gitlens
+  code --install-extension gruntfuggly.todo-tree
+) ELSE IF "%option%"=="PHP" (
+  code --install-extension gerardreches.php-extension-pack
+  code --install-extension bmewburn.vscode-intelephense-client
+  code --install-extension emallin.phpunit
+) ELSE IF "%option%"=="PYTHON" (
+  code --install-extension ms-python.python
+  code --install-extension kevinrose.vsc-python-indent
+) ELSE IF "%option%"=="WP" (
+  code --install-extension wordpresstoolbox.wordpress-toolbox
+) ELSE IF "%option%"=="CPP" (
+  code --install-extension ms-vscode.cpptools
+) ELSE (
+  echo Sorry, that is not a valid option
+  pause
+  exit
+)
